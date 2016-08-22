6eaaa78ed4ac64689d576f7d663a142dd8be6433
LuisMoralesNoriega <ingluis07@gmail.com>
2016-08-21 18:05:58 -0600

    Practica1 v4.0 Graficas

--- [SO1]Practica1_201020944/public/javascripts/infocpu.js
+++ [SO1]Practica1_201020944/public/javascripts/infocpu.js
@@ -5,8 +5,32 @@ var socket = io.connect('http://localhost:3000',{'forceNew':true});
 
 
 socket.on('rescpu',function (cpuPorcentaje) {
+    line1.append(new Date().getTime(), cpuPorcentaje);
     document.getElementById('porcentaje').innerHTML =  Math.round(cpuPorcentaje) + '%';
 });
+
+
+var smoothie = new SmoothieChart({
+    grid: {
+        strokeStyle: 'rgb(125,0,0)', fillStyle: 'rgb(255,255,255)',
+        lineWidth: 1, millisPerLine: 250, verticalSections: 6,
+    },
+    labels: {fillStyle: 'rgb(60,0,0)'}
+});
+
+smoothie.streamTo(document.getElementById("mycanvas"), 1000 /*delay*/);
+
+// Data
+var line1 = new TimeSeries();
+
+// Add to SmoothieChart
+smoothie.addTimeSeries(line1, {
+    strokeStyle: 'rgb(0,255,0)',
+    fillStyle: 'rgba(0,255,0,0.4)',
+    lineWidth: 3
+});
+//smoothie.addTimeSeries(line2);
+
 
 
 setInterval(function () {
--- [SO1]Practica1_201020944/public/javascripts/memoriaram.js
+++ [SO1]Practica1_201020944/public/javascripts/memoriaram.js
@@ -8,12 +8,38 @@ var socket = io.connect('http://localhost:3000',{'forceNew':true});
 
 
 socket.on('resram',function (memPorcentaje, memtotal, memOcupada) {
+    line1.append(new Date().getTime(), memPorcentaje);
+
     document.getElementById('totalservicio').innerHTML = memtotal;
 
     document.getElementById('totalconsumida').innerHTML = memOcupada;
 
     document.getElementById('porcentaje').innerHTML =  Math.round(memPorcentaje) + '%';
 });
+
+
+
+var smoothie = new SmoothieChart({
+    grid: {
+        strokeStyle: 'rgb(125,0,0)', fillStyle: 'rgb(255,255,255)',
+        lineWidth: 1, millisPerLine: 250, verticalSections: 6,
+    },
+    labels: {fillStyle: 'rgb(60,0,0)'}
+});
+
+smoothie.streamTo(document.getElementById("mycanvas"), 1000 /*delay*/);
+
+// Data
+var line1 = new TimeSeries();
+
+// Add to SmoothieChart
+smoothie.addTimeSeries(line1, {
+    strokeStyle: 'rgb(0,255,0)',
+    fillStyle: 'rgba(0,255,0,0.4)',
+    lineWidth: 3
+});
+//smoothie.addTimeSeries(line2);
+
 
 
 setInterval(function () {
